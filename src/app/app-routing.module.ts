0a97780c44ebbd223168847f24acb4946dd4a120
Priyal Jain <priyaljain919@gmail.com>
2020-05-05 15:37:15 +0530

    routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthLayoutComponent } from './core/layouts/auth-layout/auth-layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: './auth/auth.module#AuthModule',
+    component: AuthLayoutComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+
+
+
+@NgModule({
+  declarations: [AuthLayoutComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
