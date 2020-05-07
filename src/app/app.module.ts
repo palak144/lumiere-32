7acfd05cf6e9a9929ec40eb51eff5ceadeb71492
Priyal Jain <priyaljain919@gmail.com>
2020-05-07 15:56:11 +0530

    ui code updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './core/auth/auth.module';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     AuthModule,
+    SharedModule,
     BrowserAnimationsModule
   
   
