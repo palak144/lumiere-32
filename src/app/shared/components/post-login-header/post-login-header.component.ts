b1b32839456159f86eec127f89aaa7c3b8da3bce
palak144 <palak144>
2020-06-10 16:09:20 +0530

    orders

--- src/app/shared/components/post-login-header/post-login-header.component.ts
+++ src/app/shared/components/post-login-header/post-login-header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post-login-header',
@@ -11,7 +11,8 @@ export class PostLoginHeaderComponent implements OnInit {
   routerUrl:string;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private route : ActivatedRoute
   ) { 
     this.routerUrl = this.router.url;
     console.log('router url::', this.router.url);
@@ -19,6 +20,21 @@ export class PostLoginHeaderComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  dashboard(){
+    
+    this.router.navigate(['user/dashboard'] )
+  }
+  profile(){
+    
+    this.router.navigate(['user/profile'] )
+  }
+  orders(){
+    
+    this.router.navigate(['user/orders'] )
+  }
+  wishList(){
+    
+    this.router.navigate(['user/wishlist'] )
+  }
 }
 
