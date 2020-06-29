dd966a63a85f52b58b47ab3df6d52e372398554d
Priyal Jain <priyaljain919@gmail.com>
2020-06-29 21:10:21 +0530

    landing page updated

--- src/app/core/content-pages/pages/landing/landing.component.ts
+++ src/app/core/content-pages/pages/landing/landing.component.ts
@@ -56,6 +56,82 @@ export class LandingComponent implements OnInit {
     }
   ];
 
+  featureSlides = [
+      {
+        img: "assets/images/featured/techinasia-logo.svg"
+      },
+      {
+        img: "assets/images/featured/dentalasialogo.svg"
+      },
+      {
+        img: "assets/images/featured/e27logo.svg"
+      },
+      {
+        img: "assets/images/featured/seamlesslogo.svg"
+      },
+      {
+        img: "assets/images/featured/fundedherelogo.svg"
+      },
+      {
+        img: "assets/images/featured/seamlesslogo.svg"
+      },
+  ];
+  
+
+  customerSlides = [
+    {
+      text: 'In our village, when we were young, we used to eat bottle gourds right off the farm, and they were sweet and soft. After many decades, I have the same taste in my mouth!  when an unknown printer took a galley of type and scrambled it After many decades, I have the same taste in my mouth!  when an unknown printer took a galley of type and scrambled it printer took a galley of type and scrambled it After many decades,  when an unknown printer took a galley of type and scrambled it printer took a galley of type and scrambled it After many decades.',
+      img : 'assets/images/customer-image.svg',
+      name: 'Camila Soler'
+    },
+    {
+      text: 'In our village, when we were young, we used to eat bottle gourds right off the farm, and they were sweet and soft. After many decades, I have the same taste in my mouth!  when an unknown printer took a galley of type and scrambled it After many decades, I have the same taste in my mouth!  when an unknown printer took a galley of type and scrambled it printer took a galley of type and scrambled it After many decades,  when an unknown printer took a galley of type and scrambled it printer took a galley of type and scrambled it After many decades.',
+      img : 'assets/images/customer-image.svg',
+      name: 'Camila Soler'
+    }
+  ];
+
+  customerSlideConfig = {
+    "slidesToShow": 1, 
+    "slidesToScroll": 1,
+    "nextArrow": '<i class="fa fa-long-arrow-right"></i>',
+    "prevArrow": '<i class="fa fa-long-arrow-left"></i>',
+  };
+
+  featureSlideConfig = {
+    "slidesToShow": 5, 
+    "slidesToScroll": 5,
+    "margin": 10,
+    "nextArrow": '<i class="fa fa-chevron-right"></i>',
+    "prevArrow": '<i class="fa fa-chevron-left"></i>',
+    "dots": true,
+    // "nextArrow": '<i class="fa fa-chevron-right"></i>'
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 1008,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
+  };
+
 
   slideConfig = {
     "slidesToShow": 1, 
