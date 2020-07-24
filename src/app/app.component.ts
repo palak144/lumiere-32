d04df5bc9d645c37d1ee6d7c22aae70ba6bb1fa1
arihant <arihant.j@engineerbabu.in>
2020-07-24 18:40:19 +0530

    responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  
 })
 export class AppComponent implements OnInit {
 
 
   title = 'lumeire-angular';
+
   constructor(
     private router:Router,
   ){
